91aabca181aaa0a41b740ad2ce164a887417dd9c
thosijulio <thosijulio@outlook.com.br>
2023-01-03 16:17:41 -0300

    usando variaveis

---
+++ HelloWorld/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class HelloWorld
+ {
+  static void Main(string[] args)
+  {
+    int firstNumber = 6;
+
+    Console.WriteLine("Hello, World!");
+    Console.WriteLine(firstNumber);
+
+    firstNumber = 10;
+
+    Console.WriteLine(firstNumber);
+    Console.ReadLine();
+  }
+}
