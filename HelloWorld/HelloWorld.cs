b9c386a26c59407cbf2a683e90b661c650a12c03
thosijulio <thosijulio@gmail.com>
2023-01-05 11:03:10 -0300

    FEAT - trabalhando com numeros

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,16 +4,21 @@ class HelloWorld
  {
   static void Main(string[] args)
   {
-    double aIntNumber = 6.1565489;
-    // para setar um valor do tipo double em uma variável do tipo int, é necessário fazer a conversão explícita desse valor (casting). O contrário não se faz necessário.
-    int aDoubleNumber = (int)aIntNumber;
+    double aDoubleNumber = 6.1565489;
+
+    // para setar um valor do tipo double em uma variável do tipo float, é necessário passar o sufíxo "f" para criar um literal desse tipo.
+    float aFloatNumber = (float)aDoubleNumber;
+
+    // para setar um valor do tipo double/float em uma variável do tipo int, é necessário fazer a conversão explícita desse valor (casting). O contrário não se faz necessário.
+    int aIntNumber = (int)aDoubleNumber;
 
     // int: até 32bits; long: até 64bits; short: até 16bits.
     long aLongNumber = 1009128321834309123;
     short aShortNumer = 12324;
 
     Console.WriteLine("Hello, World!");
-    Console.WriteLine("a int number: " + aIntNumber);
+    Console.WriteLine("a int number: " + aIntNumber + "\r\na short number: " + aShortNumer + "\r\na long number: " + aLongNumber);
+    Console.WriteLine("a double number: " + aDoubleNumber + "\r\na float number: " + aFloatNumber);
     Console.ReadLine();
   }
 }
