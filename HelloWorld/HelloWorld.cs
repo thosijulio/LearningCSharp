1d4425fd081bd40fb13729d71fdc4ec2dd937caa
thosijulio <thosijulio@gmail.com>
2023-01-05 10:36:32 -0300

    FEAT - entendendo tipagem numerica

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -4,14 +4,16 @@ class HelloWorld
  {
   static void Main(string[] args)
   {
-    int firstNumber = 6;
+    double aIntNumber = 6.1565489;
+    // para setar um valor do tipo double em uma variável do tipo int, é necessário fazer a conversão explícita desse valor (casting). O contrário não se faz necessário.
+    int aDoubleNumber = (int)aIntNumber;
+
+    // int: até 32bits; long: até 64bits; short: até 16bits.
+    long aLongNumber = 1009128321834309123;
+    short aShortNumer = 12324;
 
     Console.WriteLine("Hello, World!");
-    Console.WriteLine(firstNumber);
-
-    firstNumber = 10;
-
-    Console.WriteLine(firstNumber);
+    Console.WriteLine("a int number: " + aIntNumber);
     Console.ReadLine();
   }
 }
