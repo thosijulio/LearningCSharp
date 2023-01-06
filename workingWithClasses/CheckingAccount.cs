43c507b71aa176f5e3665da51926e7645f0f77c8
thosijulio <thosijulio@gmail.com>
2023-01-06 11:03:09 -0300

    FEAT - inicio do projeto; criação da classe conta corrente e seus campos; instanciando uma classe e acessando os campos publicos a partir de outro arquivo.

---
+++ workingWithClasses/CheckingAccount.cs
@@ -0,0 +1,22 @@
+﻿/*
+	Curso de C#: Orientação a objeto (Alura).
+	Neste curso, foi desenvolvido uma aplicação para o ByteBank, um banco digital fictício, sendo que o objetivo
+	dessa aplicação será o controle de contas correntes dos clientes desse banco.
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bytebank
+{
+	class CheckingAccount
+	{
+		public int agencyCode = 0;
+		public string account = "";
+		public string owner = "";
+		public double balance = 1200;
+	}
+}
---
+++ workingWithClasses/WorkingWithClasses.cs
@@ -0,0 +1,7 @@
+﻿using bytebank;
+
+// Criação de uma instância (obj) da classe CheckingAccount.
+CheckingAccount brunoAccount = new CheckingAccount();
+
+// Acessando os campos públicos criados na classe;
+Console.WriteLine(brunoAccount.balance);
