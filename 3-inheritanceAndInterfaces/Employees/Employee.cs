aa1a185d3be003790556c25000963f2f7197c3aa
thosijulio <thosijulio@gmail.com>
2023-01-26 13:50:38 -0300

    passando parametro para o construtor da classe pai

--- 3-inheritanceAndInterfaces/Employees/Employee.cs
+++ 3-inheritanceAndInterfaces/Employees/Employee.cs
@@ -5,6 +5,13 @@ namespace Employees
         /* 
         Método construtor da classe. Não é necessário criar, pois toda classe já possui um construtor padrão.
             Sempre o construtor da classe pai será executado antes em relação ao método da classe herdeira.
+
+            Ainda sobre construtores:
+        Podemos aplicar os modificadores de acesso (visibilidade) aos construtores, como private ou protected, para a execução de uma tarefa específica
+        ou para não permitir que a classe gere instâncias (objetos);
+        
+        O construtor também é um método, portanto, podemos aplicar sobrecargas sobre ele. Assim, é possível haver mais de um construtor definido em
+        uma mesma classe e cada um deles vai se diferenciar de acordo com sua assinatura (conjunto tipo de retorno e listagem de parâmetros que recebe).
         */
         public Employee(string name, string cpf, double salary)
         {
@@ -12,11 +19,16 @@ namespace Employees
             this.Name = name;
             this.Cpf = cpf;
             this.Salary = salary;
+            ++totalEmployees;
         }
 
         public string Name { get; private set; }
         public string Cpf { get; private set; }
         public double Salary { get; private set; }
+
+        // Propriedade com a palavra reservada STATIC significa que ela pertence a classe, e não ao objeto.
+        // Para definir um valor padrão para uma propriedade, basta utilizar o sinal de igualdade após fechar chaves.
+        public static int totalEmployees { get; private set; } = 0;
 
         // o modificador "virtual" é usado para modificar um método, propriedade, index ou declaração
         // nas suas classes herdeiras. Por exemplo, o método getBonus tem esse comportamento padrão 
--- 3-inheritanceAndInterfaces/main.cs
+++ 3-inheritanceAndInterfaces/main.cs
@@ -1,13 +1,17 @@
 ﻿
+Console.WriteLine("\nCriando objeto da classe Employee");
 Employees.Employee pedro = new Employees.Employee("Pedro", "45678912310", 2000);
+Console.WriteLine("\nCriando objeto da classe Director");
 Employees.Director ana = new Employees.Director("Ana", "12345678910", 5000);
 
 Utils.BonusManager bonusManager = new Utils.BonusManager();
 
-Console.WriteLine(pedro.Salary);
-Console.WriteLine(ana.Salary);
+Console.WriteLine("\nSalário do Employee Pedro: R$" + pedro.Salary);
+Console.WriteLine("Salário da Director Ana: R$" + ana.Salary);
+
 
 bonusManager.registerBonus(pedro);
 bonusManager.registerBonus(ana);
 
-Console.WriteLine("Total de bônus concedido: " + Utils.BonusManager.TotalBonus);
+Console.WriteLine("\nTotal de bônus concedido: " + Utils.BonusManager.TotalBonus);
+Console.WriteLine("Total de Funcionários registrados: " + Employees.Employee.totalEmployees);
