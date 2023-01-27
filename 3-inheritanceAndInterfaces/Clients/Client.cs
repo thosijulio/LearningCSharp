2b3713522862b0da34843abf194c2acb51cfc445
thosijulio <thosijulio@gmail.com>
2023-01-27 11:41:49 -0300

    toda classe herda a classe Object

---
+++ 3-inheritanceAndInterfaces/Clients/Client.cs
@@ -0,0 +1,21 @@
+namespace Clients
+{
+    // A classe Object já é herdade automaticamente, não é necessário explicitar a herança. Além disso
+    // caso posteriomente a classe Client seja herdeira de outra classe, basta remover a Object e colocar a classe. As duas seriam herdadas.
+    public class Client:Object
+    {
+        public string Cpf { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public Client(string cpf, string email, string password)
+        {
+            this.Cpf = cpf;
+            this.Email = email;
+            this.Password = password;
+        }
+
+        // No c#, toda classe herda indiretamente a classe Object, então é possível sobrescrever seus métodos virtuais (ToString, Equals, GetHashCode, etc);
+        public override 
+    }
+}
