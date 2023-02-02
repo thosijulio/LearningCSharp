7906f732417c411663ec42cabea996a82c65ee6b
thosijulio <thosijulio@gmail.com>
2023-02-02 14:37:27 -0300

    arquivos iniciais do curso de C#: trabalhando com arquivos

---
+++ 5-workingWithFiles/ByteBankIO/CheckingAccount.cs
@@ -0,0 +1,43 @@
+﻿namespace ByteBankIO
+{
+    public class CheckingAccount
+    {
+        public int AccountCode { get; private set; }
+        public string AgencyName { get; private set; }
+        public double Balance { get; private set; }
+        public Client Owner { get; private set; }
+
+        public CheckingAccount(int accountCode, string agencyName, double balance, Client owner)
+        {
+            AccountCode = accountCode;
+            AgencyName = agencyName;
+            Balance = balance;
+            Owner = owner;
+        }
+
+        public void Deposit(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Valor de deposito deve ser maior que zero.", nameof(value));
+            }
+
+            Balance += value;
+        }
+
+        public void Withdraw(double value)
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Valor de saque deve ser maior que zero.", nameof(value));
+            }
+
+            if (value > Balance)
+            {
+                throw new InvalidOperationException("Saldo insuficiente.");
+            }
+
+            Balance += value;
+        }
+    }
+}
---
+++ 5-workingWithFiles/ByteBankIO/Client.cs
@@ -0,0 +1,16 @@
+﻿namespace ByteBankIO
+{
+    public class Client
+    {
+        public string Name { get; private set; }
+        public string Cpf { get; private set; }
+        public string Occupation { get; private set; }
+
+        public Client(string name, string cpf, string occupation)
+        {
+            Name = name;
+            Cpf = cpf;
+            Occupation = occupation;
+        }
+    }
+}
